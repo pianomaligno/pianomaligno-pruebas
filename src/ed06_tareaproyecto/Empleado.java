20e74854478a100127f7b90d2eb6938164c392d1
Víctor <Vctor@DESKTOP-R4MS3BJ>
2017-05-31 23:42:08 +0200

    última actualización 31 Mayo

--- src/ed06_tareaproyecto/Empleado.java
+++ src/ed06_tareaproyecto/Empleado.java
@@ -6,8 +6,12 @@
 package ed06_tareaproyecto;
 
 /**
- *
+ * Esta clase crea un objeto empleado en el cual se recogen una serie de datos de la persona. También tiene métodos para
+ * calcular el sueldo o imprimir por pantalla toda la información que tenemos.
  * @author ED06
+ * @version 1.0
+ * @see calcula_sueldo
+ * @see imprimir_emp
  */
 public class Empleado {
     private String dni;
@@ -17,6 +21,11 @@ public class Empleado {
     private int numHijos;
     private String descripcion;
     
+/** 
+ * Este es el constructor de la clase Empleado, la cual recibe dos parámetros:
+ * @param dni para insertar el dni del empleado.
+ * @param nombre donde se indica el nombre de la persona en cuestión.
+ */
     public Empleado (String dni, String nombre){
         this.dni=dni;
         this.nombre=nombre;
@@ -24,6 +33,11 @@ public class Empleado {
         this.numHijos=0;
     }
     
+    /**
+     * Este método se encarga de imprimir en pantalla los información que deseamos visualizar del empleado. Recibe por parámetro
+     * el objeto empleado del cual deseamos ver los datos.
+     * @param empleado1 
+     */
     public static void imprimir_emp(Empleado empleado1) {
         System.out.println("DNI: " + empleado1.getDni());
         System.out.println("Nombre: " + empleado1.getNombre());
@@ -32,6 +46,11 @@ public class Empleado {
         System.out.println("Sueldo: " + empleado1.calcula_sueldo(1200));
     }
     
+    /**
+     * Este método se encarga de calcular el sueldo de cada empleado en función del cargo y del número de hijos que tiene.
+     * @param base
+     * @return 
+     */
     public double calcula_sueldo(double base){
         double total=base;
         
